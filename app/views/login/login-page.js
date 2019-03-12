c1216c5e3931702dddd30982ce0edfb682fa0902
Levi Donaldson <lcdbacanas@gmail.com>
2019-03-11 23:05:33 -0500

    login page edits

---
+++ app/views/login/login-page.js
@@ -0,0 +1,25 @@
+
+let page;
+let email;
+
+let observableModule = require("tns-core-modules/data/observable");
+
+let user = new observableModule.fromObject({
+    email: "nativescriptrocks@progress.com",
+    password: "password"
+});
+
+exports.loaded = ((args) => {
+    page = args.object;
+    page.bindingContext = user;
+});
+
+exports.createAccount = (() => {
+    email = page.getViewById("password");
+    console.log(password.text);
+});
+
+exports.logIn = (() => {
+    email = page.getViewById("email");
+    console.log(email.text);
+});
