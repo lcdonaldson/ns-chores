0b038ff58f6c2576142b79f472ef260e661c6033
Levi Donaldson <lcdbacanas@gmail.com>
2019-03-13 21:41:36 -0500

    worked on login page and added simple backend

--- app/views/login/login-page.js
+++ app/views/login/login-page.js
@@ -1,25 +1,50 @@
+let dialogsModule = require("tns-core-modules/ui/dialogs");
+let frameModule = require("tns-core-modules/ui/frame");
+
+let UserViewModel = require("../common/view-models/user-view-model");
+
+let user = new UserViewModel({
+    email: "dummyUser@email.com",
+    password: "pass"
+});
 
 let page;
-let email;
-
-let observableModule = require("tns-core-modules/data/observable");
-
-let user = new observableModule.fromObject({
-    email: "nativescriptrocks@progress.com",
-    password: "password"
-});
 
 exports.loaded = ((args) => {
     page = args.object;
+    page.actionBarHidden = true;
     page.bindingContext = user;
 });
 
-exports.createAccount = (() => {
-    password = page.getViewById("password");
-    console.log(password.text);
+exports.signIn = (() => {
+    user.login()
+    .catch(function (error) {
+        console.log(error);
+        dialogsModule.alert({
+            message: "Unfortunately we could not find your account.",
+            okButtonText: "OK"
+        });
+        return Promise.reject();
+    })
+    .then(function () {
+        frameModule.topmost().navigate("views/home/home-page");
+    });
 });
 
-exports.logIn = (() => {
-    email = page.getViewById("email");
-    console.log(email.text);
+exports.register = (() => {
+    user.register()
+    .then(function () {
+        dialogsModule
+            .alert("Your account was successfully created.")
+            .then(function () {
+                alert("you need a better way to let users know they're registered here.")
+                // exports.toggleDisplay();
+            });
+    }).catch(function (error) {
+        dialogsModule
+            .alert({
+                message: "Unfortunately we were unable to create your account.",
+                okButtonText: "OK"
+            });
+    });
 });
